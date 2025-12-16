c40723b679b434d47f409230e77399d6c3ef37cc
Krzysztof Wacławik <krzysztof.waclawik.kao@gmail.com>
2025-12-16 19:08:17 +0100

    Changing collector of unused statements to list - map is not used in practice and list helps with maintaining encounter order. Small cleanup as tests are starting to develop

--- src/main/java/com/kw/parserProject/UnusedStatementChecker.java
+++ src/main/java/com/kw/parserProject/UnusedStatementChecker.java
@@ -8,26 +8,20 @@ import java.util.stream.Stream;
 public class UnusedStatementChecker {
 
     List<Statement> getUnusedStatements(List<Statement> program) {
-        Map<String, Stack<Statement>> unusedOverwrittenAssignmentsFromStatements = new HashMap<>();
+        List<Statement> unusedOverwrittenAssignmentsFromStatements = new LinkedList<>();
         Map<String, Statement> recentAssignments = new HashMap<>();
 
         for (Statement parsedAssignment : program) {
             updateUnusedStatements(unusedOverwrittenAssignmentsFromStatements, recentAssignments, parsedAssignment);
         }
 
-        return Stream.concat(unusedOverwrittenAssignmentsFromStatements.values().stream().filter(stack -> !stack.isEmpty()).flatMap(Collection::stream),
-                        recentAssignments.values().stream())
-                .toList();
+        return Stream.concat(
+                        unusedOverwrittenAssignmentsFromStatements.stream(),
+                        recentAssignments.values().stream()
+                ).toList();
     }
 
-    void updateUnusedStatements(Map<String, Stack<Statement>> unusedStatements, Map<String, Statement> recentAssignments, Statement statement) {
-        //TODO can't use stack alone, as smth like:
-        // b = 1
-        // b = 2
-        // a = b + 1
-        // c = b + 2
-        // will clear two writes of b, and it should only one. Write test against it
-
+    private void updateUnusedStatements(List<Statement> unusedStatements, Map<String, Statement> recentAssignments, Statement statement) {
         // let's start pre-traversal first
         // if variable not in recent assignments, nothing to do, if it's there - let's clear the flag
         statement.readVariables().forEach(recentAssignments::remove);
@@ -41,7 +35,7 @@ public class UnusedStatementChecker {
             Statement previousStatement = recentAssignments.get(writeVariable);
             if (previousStatement != null) {
                 // value was written to, but never read up to this point, let's store it
-                unusedStatements.computeIfAbsent(statement.writeVariable(), (_) -> new Stack<>()).push(previousStatement);
+                unusedStatements.add(previousStatement);
             }
             // let's populate recent assignments with the freshest entry
             recentAssignments.put(writeVariable, statement);
